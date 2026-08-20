ecea394d1610a12e50f5836a481b1a553e037f2c
SpdrSO <spdrso.git@gmail.com>
2026-08-20 21:17:11 +0300

    strings

--- script.go
+++ script.go
@@ -41,6 +41,18 @@ type Config struct {
 		Version string `toml:"version"`
 		Loader  string `toml:"loader"`
 	} `toml:"meta"`
+	Strings struct {
+		ServerModsHeader           string `toml:"server_mods_header"`
+		ServerModsDescription      string `toml:"server_mods_description"`
+		ServerLoaderModsHeader     string `toml:"server_loader_mods_header"`
+		ServerDatapacksHeader      string `toml:"server_datapacks_header"`
+		RequiredModsHeader         string `toml:"required_mods_header"`
+		RequiredModsDescription    string `toml:"required_mods_description"`
+		RecommendedModsHeader      string `toml:"recommended_mods_header"`
+		RecommendedModsDescription string `toml:"recommended_mods_description"`
+		OptionalModsHeader         string `toml:"optional_mods_header"`
+		OptionalModsDescription    string `toml:"optional_mods_description"`
+	} `toml:"strings"`
 	List struct {
 		Server      []Mod `toml:"server"`
 		Required    []Mod `toml:"required"`
